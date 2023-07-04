a38601bacf993cfdc0cff0a07142ef922713402c
Abigail Haddad <abigail.haddad@gmail.com>
2023-07-04 09:50:17 -0400

    -fetching qmd files one directory up from before

--- code/fetch_shiny_docs.py
+++ code/fetch_shiny_docs.py
@@ -1,7 +1,6 @@
 import requests
 import os
 import re
-import shutil
 
 def remove_svg_and_png_tags(text):
     cleaned_text = re.sub(r'<svg.*?</svg>', '', text, flags=re.DOTALL)
@@ -31,10 +30,35 @@ def remove_empty_folders(root_folder):
         if not os.listdir(foldername) and foldername != root_folder:
             os.rmdir(foldername)
 
-def fetch_files_from_directory(path, output_folder="../data"):
+def fetch_files_from_directory(output_folder="../data"):
     # Ensure output folder exists
     os.makedirs(output_folder, exist_ok=True)
 
+    # Send a request to the GitHub API
+    response = requests.get('https://api.github.com/repos/rstudio/py-shiny-docs/contents')
+    response.raise_for_status()  # ensure we notice bad responses
+
+    for item in response.json():
+        if item['type'] == 'file' and item['name'].endswith('.qmd'):
+            # Download file
+            file_content = requests.get(item['download_url']).text
+
+            # Write file to disk
+            filename = os.path.join(output_folder, item['name'].replace('.qmd', '.txt'))
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(file_content)
+
+        elif item['type'] == 'dir':
+            # Recursively call for directories
+            fetch_files_from_directory_in_subfolder(item['path'], output_folder)
+
+    # Clean and move files to main output folder
+    flatten_directory_structure(output_folder, output_folder)
+
+    # Remove empty folders
+    remove_empty_folders(output_folder)
+
+def fetch_files_from_directory_in_subfolder(path, output_folder):
     # Send a request to the GitHub API
     response = requests.get(f'https://api.github.com/repos/rstudio/py-shiny-docs/contents/{path}')
     response.raise_for_status()  # ensure we notice bad responses
@@ -49,17 +73,9 @@ def fetch_files_from_directory(path, output_folder="../data"):
             with open(filename, 'w', encoding='utf-8') as f:
                 f.write(file_content)
 
-
         elif item['type'] == 'dir':
-            # Create new output folder for subdirectory
-            new_output_folder = os.path.join(output_folder, item['name'])
-            fetch_files_from_directory(item['path'], output_folder=new_output_folder)  # recursive call for directories
-
-    # Clean and move files to main output folder
-    flatten_directory_structure(output_folder, output_folder)
-
-    # Remove empty folders
-    remove_empty_folders(output_folder)
+            # Recursively call for directories
+            fetch_files_from_directory_in_subfolder(item['path'], output_folder)
 
 # Start fetching files
-fetch_files_from_directory('docs')
+fetch_files_from_directory()
